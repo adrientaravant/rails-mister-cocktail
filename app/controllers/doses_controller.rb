68340d8a54388057946ab89d359e0a3f7c49ff35
adrien taravant <adrien.taravant@gmail.com>
2017-02-01 17:24:56 +0100

    views done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,27 @@
 class DosesController < ApplicationController
+
+  before_action :set_cocktail
+
+  def create
+    # @ingredient = Ingredient.find(dose_params["ingredient"].to_i)
+    # @dose = Dose.new(description: dose_params["description"], ingredient: @ingredient, cocktail: @cocktail)
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail)
+    else
+      raise
+      render 'cocktails/show'
+    end
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,11 @@
 require 'open-uri'
 require 'json'
 
-
 url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-
 
 serialized_ingredients = open(url).read
 ingredients = JSON.parse(serialized_ingredients)
 
-i = 0
 ingredients["drinks"].each do |ingredient|
   Ingredient.create!(name: ingredient["strIngredient1"])
 end
