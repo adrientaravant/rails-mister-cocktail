b0c358509486cfb6da62dc949acddaf8744b822c
adrien taravant <adrien.taravant@gmail.com>
2017-02-01 14:51:07 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :ingredients
-  root 'ingredients#index'
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+
+  resources :doses, only: [:destroy]
+
+  root 'cocktails#index'
+
 end
